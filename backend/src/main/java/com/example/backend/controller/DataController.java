129bb2904346e4e2b5f3251523ea35ff6adafa09
lender512 <luis.berrospi@utec.edu.pe>
2021-07-10 11:13:39 -0500

    CourseDTO and controller

---
+++ backend/src/main/java/com/example/backend/controller/DataController.java
@@ -0,0 +1,38 @@
+package com.example.backend.controller;
+
+
+import com.example.backend.model.CourseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequiredArgsConstructor
+public class DataController {
+
+    @GetMapping("/data")
+    @PreAuthorize("hasRole('USER')")
+    public Map<String, String> getData(@RequestBody CourseDTO courseDTO) {
+        HashMap<String, String> course = new HashMap<>();
+        course.put("course",courseDTO.getName());
+
+        HttpEntity<HashMap<String, String>> request = new HttpEntity<>(course);
+
+        var restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("Prueba", "Prueba"));
+        String response = restTemplate.postForObject("http://3.143.255.114/api/numerical/data", request, String.class);
+
+        HashMap<String, String> result = new HashMap<>();
+        result.put("response", response);
+        return result;
+    }
+}
