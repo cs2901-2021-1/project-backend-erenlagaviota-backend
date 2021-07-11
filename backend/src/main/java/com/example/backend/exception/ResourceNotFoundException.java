6884b8035134eab26b3b8ac72bd2185ee9f94252
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-11 18:14:11 -0500

    fix

--- backend/src/main/java/com/example/backend/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/example/backend/exception/ResourceNotFoundException.java
@@ -7,14 +7,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Data
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    private final String resourceName;
-    private final String fieldName;
-    private final transient Object fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-        this.resourceName = resourceName;
-        this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
     }
 }
