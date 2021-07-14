fe4d11b25cac0aec802b7d477bab4c8751257c7c
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-14 09:20:25 -0500

    Course-Controller working

--- backend/src/main/java/com/example/backend/controller/CourseController.java
+++ backend/src/main/java/com/example/backend/controller/CourseController.java
@@ -1,5 +1,30 @@
 package com.example.backend.controller;
 
+
+import com.example.backend.config.Constants;
+import com.nimbusds.jose.shaded.json.JSONArray;
+
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+import lombok.RequiredArgsConstructor;
+
+@RestController()
+@RequiredArgsConstructor
+@RequestMapping("/courses")
 public class CourseController {
-    
+    @GetMapping("/valid")
+    @PreAuthorize("hasRole('USER')")
+    public JSONArray getValidCourses(){
+        var restTemplate = new RestTemplate();
+        restTemplate.getInterceptors().add(new BasicAuthenticationInterceptor("prueba", "prueba"));
+        JSONArray response = new JSONArray();
+        response = restTemplate.getForObject(Constants.ENDPOINT_URL + "api/cursos/valid", response.getClass());
+        return response;
+    }
 }
--- backend/src/main/java/com/example/backend/model/api/CourseDetails.java
+++ backend/src/main/java/com/example/backend/model/api/CourseDetails.java
@@ -1,3 +1,31 @@
 package com.example.backend.model.api;
 
+public class CourseDetails {
+    public String getCodcurso() {
+        return codcurso;
+    }
 
+    public void setCodcurso(String codcurso) {
+        this.codcurso = codcurso;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String codcurso;
+    private String department;
+    private String name;
+}
