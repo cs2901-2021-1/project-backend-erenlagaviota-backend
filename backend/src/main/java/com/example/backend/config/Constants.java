0f0e677657c415cb6a786e8e0a59a55954bc1924
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-23 01:25:25 -0500

    cache BETA

--- backend/src/main/java/com/example/backend/config/Constants.java
+++ backend/src/main/java/com/example/backend/config/Constants.java
@@ -1,7 +1,7 @@
 package com.example.backend.config;
 
 public final class Constants {
-    public static final String ENDPOINT_URL = "http://3.143.255.114/";
+    public static final String ENDPOINT_URL = "http://18.117.8.76/";
 
     private Constants(){
 
