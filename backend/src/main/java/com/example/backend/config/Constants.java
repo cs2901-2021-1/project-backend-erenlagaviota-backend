661cded40e50d8ce6fcf97cda0c893c801e16cfb
morphisjustfun <mariojacobo2011@gmail.com>
2021-07-23 07:44:39 -0500

    try try

--- backend/src/main/java/com/example/backend/config/Constants.java
+++ backend/src/main/java/com/example/backend/config/Constants.java
@@ -1,7 +1,7 @@
 package com.example.backend.config;
 
 public final class Constants {
-    public static final String ENDPOINT_URL = "http://18.117.8.76/";
+    public static final String ENDPOINT_URL = "http://ec2-18-117-8-76.us-east-2.compute.amazonaws.com/";
 
     private Constants(){
 
